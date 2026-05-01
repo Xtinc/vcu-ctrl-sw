b30e78e7ecebdb1e636068152c6991ca8e7cc4ed
Xtinc <remingtonscarlet@outlook.com>
2026-05-01 04:57:36 +0100

    update cmake

--- cmake/options.cmake
+++
@@ -1,8 +0,0 @@
-# Feature flags matching config.mk and codec_defs.mk defaults
-
-option(ENABLE_ROI               "Enable Region-of-Interest support"    ON)
-option(ENABLE_MICROBLAZE        "Enable Microblaze MCU firmware path"  ON)
-option(ENABLE_HIGH_DYNAMIC_RANGE "Enable HDR support"                  ON)
-option(ENABLE_ENC_SW_MULTIPASS  "Enable encoder software multi-pass"   ON)
-option(ENABLE_LINUX_DRIVER      "Enable Linux DMA allocator driver"    ON)
-option(ENABLE_64BIT             "Enable 64-bit pointer/address support" ON)
